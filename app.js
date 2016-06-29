74b27185aa144c066abe62a9ee26f752e306219e
Mason Pierce <masopie@gmail.com>
2016-06-29 10:33:02 -0500

    Put color var back into each event. Will not change if set to global initially

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-hueHueHue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
-
 $(document).ready(function(){
     console.log("Work complete!");
     $("p").mousedown(function(event){
       event.preventDefault();
+      var hueHueHue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
       $(this).css("color", hueHueHue)
       // $("#boop").css()
     })
     $("p").mouseup(function(event){
       event.preventDefault();
+      var hueHueHue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
       $(this).css("color", hueHueHue)
       // $("#boop").css()
     })
