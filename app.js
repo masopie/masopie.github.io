3ea7ef725a6a7f8078d3552bf4be15edc0d760bb
Mason Pierce <masopie@gmail.com>
2016-06-29 10:31:38 -0500

    Pull out rando color var

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 hueHueHue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
+
 $(document).ready(function(){
     console.log("Work complete!");
     $("p").mousedown(function(event){
